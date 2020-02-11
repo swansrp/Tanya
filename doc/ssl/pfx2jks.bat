004ce4dbae0cc2927490d4de759b25c2abd16d64
sharp <56093273@qq.com>
2020-02-11 21:07:53 +0800

    更新ssl证书

--- doc/ssl/pfx2jks.bat
+++ doc/ssl/pfx2jks.bat
@@ -1,2 +1,2 @@
-keytool -importkeystore -srckeystore  1873910_api.tanyakeji.com.pfx -srcstoretype pkcs12 -destkeystore tanya.jks -deststoretype JKS
+keytool -importkeystore -srckeystore  3447479_api.tanyakeji.com.pfx -srcstoretype pkcs12 -destkeystore tanya.jks -deststoretype JKS
 pause
